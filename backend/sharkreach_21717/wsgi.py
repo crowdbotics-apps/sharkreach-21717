5c5b13cb2de83b8c547f53f8035cb7a1707fc969
Crowdbotics <team@crowdbotics.com>
2020-10-19 19:18:58 +0000

    Update models

--- backend/sharkreach_21717/wsgi.py
+++ backend/sharkreach_21717/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sharkreach_21717.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sharkreach_21717.settings")
 
 application = get_wsgi_application()
