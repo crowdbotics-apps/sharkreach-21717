d1fd70b756603ba419b8742cf749dbc2a17dcb99
Crowdbotics <team@crowdbotics.com>
2020-10-19 19:19:31 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn2150056', human_name: 'Sign In 2', access_route: 'SignIn2150056'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
